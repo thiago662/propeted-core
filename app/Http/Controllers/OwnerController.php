765017838229b6c6a6938108a73baae71a600314
thiago662 <thiago1santos12@gmail.com>
2022-08-06 01:32:46 -0300

    feat: criando controller de owner
    Resolve: [VET-11]

---
+++ app/Http/Controllers/OwnerController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Owner;
+use Illuminate\Http\Request;
+
+class OwnerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        return Owner::paginate($request->per_page);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Owner::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Owner  $owner
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Owner $owner)
+    {
+        return $owner;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Owner  $owner
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Owner $owner)
+    {
+        $owner->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Owner  $owner
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Owner $owner)
+    {
+        $owner->delete();
+    }
+}
