7a39953338a6c6a96bccd2a0483d881ef08869d4
thiago662 <thiago1santos12@gmail.com>
2022-08-06 01:25:38 -0300

    feat: criando seeder de role
    Resolve: [VET-10]

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            [
+                'id' => 1,
+                'title' => 'master',
+                'active' => true,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ]);
+    }
+}
